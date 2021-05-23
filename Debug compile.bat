99ee4defdfcd91405b3df992734e766a896fb07e
cracker1000 <ashishpurohit271@gmail.com>
2021-05-23 09:49:30 +0530

    23.3 Automate debug and static compilation of mod. Fix texter fonts. Add CEXP. Update wiki.

--- Debug compile.bat
+++ Debug compile.bat
@@ -1,2 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat""
 cd /d C:\Newmod&cd build-debug&ninja
+cmd /k
--- src/simulation/elements/CEXP.cpp
+++ src/simulation/elements/CEXP.cpp
@@ -6,7 +6,7 @@ void Element::Element_CEXP()
 {
 	Identifier = "DEFAULT_PT_CEXP";
 	Name = "CEXP";
-	Colour = PIXPACK(0x8B0000);
+	Colour = PIXPACK(0xFF6347);
 	MenuVisible = 1;
 	MenuSection = SC_EXPLOSIVE;
 	Enabled = 1;
@@ -26,10 +26,10 @@ void Element::Element_CEXP()
 	Meltable = 0;
 	Hardness = 10;
 	Weight = 100;
-	PhotonReflectWavelengths = 0x8B0000;
+	PhotonReflectWavelengths = 0xFF6347;
 
 	HeatConduct = 240;
-	Description = "Custom explosive, Tmp = Temp at which exp. occur, Tmp2 = Temp. on explosion, Ctype = Element, Life = Pressure.";
+	Description = "Custom explosive, read wiki. Tmp = Temp at which exp. occur, Tmp2 = Temp. on explosion, Ctype = Element, Life = Pressure.";
 
 	Properties = TYPE_SOLID | PROP_NEUTPENETRATE;
 
@@ -47,6 +47,12 @@ void Element::Element_CEXP()
 static int update(UPDATE_FUNC_ARGS)
 {
 	//checks
+	if (parts[i].pavg[0] == 5 )
+	{
+		sim->pv[(y / CELL)][(x / CELL)] = parts[i].life;
+		sim->kill_part(i);
+	}
+	
 	if (parts[i].life <= -250 || parts[i].life > 270)
 	{
 		parts[i].life = 270;
@@ -70,21 +76,23 @@ static int update(UPDATE_FUNC_ARGS)
 				r = pmap[y + ry][x + rx];
 				if (!r)
 					continue;
-					if (parts[i].temp - 273.15f > parts[i].tmp)
-					{
-						sim->pv[(y / CELL)][(x / CELL)] = parts[i].life;
-						sim->part_change_type(i, x + rx, y + ry, parts[i].ctype);
-						parts[ID(r)].temp = parts[i].tmp2;
-						
-					}
+				if (parts[i].temp - 273.15f > parts[i].tmp)
+				{
+					parts[ID(r)].temp = parts[i].tmp2 + 273.15f;
+					sim->create_part(-1, x, y + 1, parts[i].ctype);
+					sim->create_part(-1, x, y - 1, parts[i].ctype);
+					sim->create_part(-1, x+1, y, parts[i].ctype);
+					sim->create_part(-1, x-1, y, parts[i].ctype);
+					parts[i].pavg[0] = 5;
 				}
+			}
 	return 0;
 }
 
 static void create(ELEMENT_CREATE_FUNC_ARGS)
 {
-	    sim->parts[i].tmp = 450;
-		sim->parts[i].tmp2 = 1000;
-		sim->parts[i].ctype = PT_FIRE;
-		sim->parts[i].life = 10.0;
+	sim->parts[i].tmp = 400;
+	sim->parts[i].tmp2 = 1000;
+	sim->parts[i].ctype = PT_FIRE;
+	sim->parts[i].life = 10.0;
 }
