12eee1967f446351b5d9b5bae84c9943c2dff5f6
Cracker1000 <ashishpurohit271@gmail.com>
2021-06-05 22:00:10 +0530

    Fix GLOW

--- src/simulation/elements/GLOW.cpp
+++ src/simulation/elements/GLOW.cpp
@@ -51,23 +51,23 @@ void Element::Element_GLOW()
 static int update(UPDATE_FUNC_ARGS)
 {
 	int r, rx, ry;
-	for (rx=-1; rx<2; rx++)
-		for (ry=-1; ry<2; ry++)
+	for (rx = -1; rx < 2; rx++)
+		for (ry = -1; ry < 2; ry++)
 			if (BOUNDS_CHECK && (rx || ry))
 			{
-				r = pmap[y+ry][x+rx];
+				r = pmap[y + ry][x + rx];
 				if (!r)
 					continue;
-				if (TYP(r)==PT_WATR && RNG::Ref().chance(1, 400))
+				if (TYP(r) == PT_WATR && RNG::Ref().chance(1, 400))
 				{
 					sim->kill_part(i);
-					sim->part_change_type(ID(r),x+rx,y+ry,PT_DEUT);
+					sim->part_change_type(ID(r), x + rx, y + ry, PT_DEUT);
 					parts[ID(r)].life = 10;
 					return 1;
 				}
 			}
-	parts[i].ctype = int(sim->pv[y/CELL][x/CELL]*16);
-	parts[i].tmp = abs((int)((sim->vx[y/CELL][x/CELL]+sim->vy[y/CELL][x/CELL])*16.0f)) + abs((int)((parts[i].vx+parts[i].vy)*64.0f));
+	parts[i].ctype = int(sim->pv[y / CELL][x / CELL] * 16);
+	parts[i].tmp = abs((int)((sim->vx[y / CELL][x / CELL] + sim->vy[y / CELL][x / CELL])*16.0f)) + abs((int)((parts[i].vx + parts[i].vy)*64.0f));
 
 	return 0;
 }
@@ -75,17 +75,26 @@ static int update(UPDATE_FUNC_ARGS)
 static int graphics(GRAPHICS_FUNC_ARGS)
 {
 
-	*firea = int(restrict_flt(64.0f+cpart->ctype, 0, 255));
-	*firer = int(restrict_flt(cpart->temp-(275.13f+32.0f), 0, 128)/50.0f);
-	*fireg = int(restrict_flt(float(cpart->ctype), 0, 128)/50.0f);
-	*fireb = int(restrict_flt(float(cpart->tmp), 0, 128)/50.0f);
+	*firer = 16 + int(restrict_flt(cpart->temp - (275.13f + 32.0f), 0, 128) / 2.0f);
+	*fireg = 16 + int(restrict_flt(float(cpart->ctype), 0, 128) / 2.0f);
+	*fireb = 16 + int(restrict_flt(float(cpart->tmp), 0, 128) / 2.0f);
+	*firea = 64;
 
-	*colr = int(restrict_flt(64.0f+cpart->temp-(275.13f+32.0f), 0, 255));
-	*colg = int(restrict_flt(64.0f+cpart->ctype, 0, 255));
-	*colb = int(restrict_flt(64.0f+cpart->tmp, 0, 255));
+	*colr = int(restrict_flt(64.0f + cpart->temp - (275.13f + 32.0f), 0, 255));
+	*colg = int(restrict_flt(64.0f + cpart->ctype, 0, 255));
+	*colb = int(restrict_flt(64.0f + cpart->tmp, 0, 255));
 
-	*pixel_mode |= FIRE_ADD;
-	*pixel_mode |= PMODE_GLOW | PMODE_ADD;
-	*pixel_mode &= ~PMODE_FLAT;
+	int rng = RNG::Ref().between(1, 32); //
+	if (((*colr) + (*colg) + (*colb)) > (256 + rng)) {
+		*colr -= 54;
+		*colg -= 54;
+		*colb -= 54;
+		*pixel_mode |= FIRE_ADD;
+		*pixel_mode |= PMODE_GLOW | PMODE_ADD;
+		*pixel_mode &= ~PMODE_FLAT;
+	}
+	else {
+		*pixel_mode |= PMODE_BLUR;
+	}
 	return 0;
 }
