d82e46554ead7620210054ae5047393c0771e55d
Cracker1000 <ashishpurohit271@gmail.com>
2022-04-03 22:38:10 +0530

    Add Napalm

---
+++ src/simulation/elements/NAPM.cpp
@@ -0,0 +1,110 @@
+#include "simulation/ElementCommon.h"
+
+static int update(UPDATE_FUNC_ARGS);
+void Element::Element_NAPM()
+{
+
+	Identifier = "DEFAULT_PT_NAPM";
+	Name = "NAPM";
+	Colour = PIXPACK(0xFF8C00);
+	MenuVisible = 1;
+	MenuSection = SC_EXPLOSIVE;
+	Enabled = 1;
+
+	Advection = 0.2f;
+	AirDrag = 0.02f * CFDS;
+	AirLoss = 0.77f;
+	Loss = 0.60f;
+	Collision = 0.0f;
+	Gravity = 0.5f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 1;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 5;
+
+	Weight = 20;
+	HeatConduct = 255;
+	Description = "Napalm, sticky liquid that burns around 1200C. Once ignited cannot be extinguished.";
+
+	Properties = TYPE_LIQUID | PROP_LIFE_DEC;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+
+	Update = &update;
+}
+
+static int update(UPDATE_FUNC_ARGS)
+{
+	if (parts[i].life > 1)
+	{
+		parts[i].vx = 0;
+		parts[i].vy = 0;
+	}
+
+	if (parts[i].tmp == 1)
+	{
+		sim->pv[(y / CELL)][(x / CELL)] = 2.0f;
+		if (parts[i].temp < 1200 + 273.15f)
+		{
+			parts[i].temp += 23.15f;
+		}
+		if (RNG::Ref().chance(1, 25))
+		{
+			sim->create_part(-1, x, y - 1, PT_FIRE);
+		}
+		if (RNG::Ref().chance(1, 300))
+		{
+			sim->create_part(-1, x, y - 1, PT_SMKE);
+		}
+		if (RNG::Ref().chance(1, 700))
+		{
+			sim->part_change_type(i, x, y, PT_FIRE);
+		}
+	}
+	int r, rx, ry;
+	for (rx = -2; rx < 3; rx++)
+		for (ry = -2; ry < 3; ry++)
+			if (BOUNDS_CHECK && (rx || ry))
+			{
+				r = pmap[y + ry][x + rx];
+				if (!r)
+					r = sim->photons[y + ry][x + rx];
+				if (!r)
+					continue;
+				if (parts[ID(r)].type != PT_NAPM && parts[ID(r)].type != PT_FIRE)
+				{
+					parts[i].life = 4;
+				}
+				switch (TYP(r))
+				{
+				case PT_FIRE:
+				case PT_SMKE:
+				case PT_PLSM:
+				case PT_SPRK:
+				{
+				 parts[i].tmp = 1;
+				}
+				break;
+				case PT_NAPM:
+				{
+					if (parts[ID(r)].tmp > 0)
+					{
+						parts[i].tmp = 1;
+					}
+				}
+				break;
+				}
+			}
+	return 0;
+}
