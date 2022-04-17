688600ccd4c6b17b5e81b14471a76eed9240795b
Cracker1000 <ashishpurohit271@gmail.com>
2022-04-17 10:08:55 +0530

    Revert back last few commits

--- src/common/Platform.cpp
+++ src/common/Platform.cpp
@@ -532,15 +532,17 @@ int main(int argc, char *argv[]);
 int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	int argc;
-	std::vector<ByteString> argv;
-	std::vector<char *> argp;
 	wchar_t **wargv = CommandLineToArgvW(GetCommandLineW(), &argc);
+	std::vector<ByteString> argv;
 	for (auto i = 0; i < argc; ++i)
 	{
 		argv.push_back(Platform::WinNarrow(std::wstring(wargv[i])));
-		argp.push_back(&argv.back()[0]);
 	}
-	LocalFree(wargv);
+	std::vector<char *> argp;
+	for (auto &arg : argv)
+	{
+		argp.push_back(&arg[0]);
+	}
 	return main(argc, &argp[0]);
 }
 #endif
