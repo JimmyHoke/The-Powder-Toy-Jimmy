4280808b8792901acddd1b18c1786e6801553259
Cracker1000 <ashishpurohit271@gmail.com>
2022-07-23 15:06:10 +0530

    Released

--- src/lua/luascripts/crackerk.lua
+++ src/lua/luascripts/crackerk.lua
@@ -1,7 +1,7 @@
 --Cracker1000 mod interface script--
 failsafe = 1 -- Meant to be a global variable, used for detecting script crash
 local passreal = "12345678"
-local crackversion = 37.0
+local crackversion = 38.0
 local passreal2 = "DMND"
 local multiplayerversion = 28
 local motw = "."
